f4f1c66319a69f1d3d34512f3c56249e092f2b84
unknown <shaharil85@gmail.com>
2021-08-07 20:02:31 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+//const config: SocketIoConfig = { url: 'https://?????.herokuapp.com/', options: {} };
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SocketIoModule.forRoot(config)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Socket } from 'ngx-socket-io';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  message = '';
+  messages = [];
+  currentUser = '';
 
-  constructor() {}
+  constructor(private socket: Socket, private toastCtrl: ToastController) {}
+  
+  ngOnInit(){
+    this.socket.connect();  
+
+    let name = `user-${new Date().getTime()}`;
+    this.currentUser = name;
+    //this.currentUser = 'shaharil';
+    this.socket.emit('set-name', this.currentUser);
+    this.socket.fromEvent('users-changed').subscribe(data => {
+      let user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left: ' + user);
+      } else {
+        this.showToast('User joined: ' + user);
+        console.log('User joined: ' + user);
+        
+      }
+    });
+    this.socket.fromEvent('message').subscribe(message => {
+      this.messages.push(message);
+    });
+  }
+  async showToast(msg) {
+    let toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  sendMessage() {
+    this.socket.emit('send-message', { text: this.message });
+    this.message = '';
+  }
+ 
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
+  
+
+
+  
 
 }
